c48bb916347cf4f5e41c6acdba362436aee11063
hamdiboyraz <hamdiboyraz19@gmail.com>
2023-06-26 16:11:21 +0300

    Fix issue#1 : Convert api to minimal api

---
+++ Back End/MemorizeWords/MemorizeWords/Api/ApplicationVariableApiInitializer.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace MemorizeWords.Api
+{
+    public static class ApplicationVariableApiInitializer
+    {
+        public static void Initialize(WebApplication app, IConfiguration configuration) {
+
+            app.MapGet("/api/ApplicationVariable", () =>
+            {
+                var sequentTrueAnswerCount = configuration.GetValue<int>("SequentTrueAnswerCount");
+
+                var applicationVariables = new
+                {
+                    SequentTrueAnswerCount = sequentTrueAnswerCount
+                };
+
+                return Results.Ok(applicationVariables);
+            });
+
+        }
+    }
+}
