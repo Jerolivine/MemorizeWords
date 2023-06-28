cc98d7f9924e54543a1c6e2f3e3aff12838a703f
Arda Şentunalı <asentunali@gmail.com>
2023-06-28 11:06:12 +0200

    22 refactor

--- Front End/memorizewords/src/app/app.component.ts
+++ Front End/memorizewords/src/app/app.component.ts
@@ -1,21 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { environment } from './../environments/environment';
-import { ExceptionService } from './services/http/exception.service';
-
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
-  title = environment.title;
-
-  constructor(private exceptionService:ExceptionService){
-
-  }
-  
-  ngOnInit(): void {
-    this.exceptionService.businessException().subscribe();
-  }
+export class AppComponent {
 }
