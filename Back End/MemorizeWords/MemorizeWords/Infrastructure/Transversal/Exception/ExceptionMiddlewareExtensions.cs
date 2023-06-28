0b82485f32cec1dcfb8b30c4bc9d995cd05e8c2f
Arda Şentunalı <asentunali@gmail.com>
2023-06-28 11:16:20 +0200

    22 - refactor

---
+++ Back End/MemorizeWords/MemorizeWords/Infrastructure/Transversal/Exception/ExceptionMiddlewareExtensions.cs
@@ -0,0 +1,8 @@
+﻿namespace MemorizeWords.Infrastructure.Transversal.Exception
+{
+    public static class ExceptionMiddlewareExtensions
+    {
+        public static IApplicationBuilder AddExceptionMiddieware(this WebApplication app)
+            => app.UseMiddleware<ExceptionMiddleware>();
+    }
+}
