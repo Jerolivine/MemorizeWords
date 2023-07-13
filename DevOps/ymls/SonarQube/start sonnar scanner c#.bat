ac794f74ab2da038220318ae837652a9431a2dbe
asentunali <asentunali@gmail.com>
2023-07-13 12:26:42 +0200

    52 - new volume

--- DevOps/ymls/SonarQube/start sonnar scanner c#.bat
+++ DevOps/ymls/SonarQube/start sonnar scanner c#.bat
@@ -5,6 +5,6 @@ cd "Back End"
 cd MemorizeWords
 cd MemorizeWords
 dotnet tool install --global dotnet-sonarscanner
-dotnet sonarscanner begin /k:"MemorizeWords" /d:sonar.host.url="http://127.0.0.1:9000"  /d:sonar.token="sqp_5ed4e463f092c3fb270d0af53fc57047ea7ad634"
+dotnet sonarscanner begin /k:"MemorizeWords" /d:sonar.host.url="http://127.0.0.1:9000"  /d:sonar.token="sqp_62bc1789216859d89b6c6b918c41a92010440305"
 dotnet build
-dotnet sonarscanner end /d:sonar.token="sqp_5ed4e463f092c3fb270d0af53fc57047ea7ad634"
+dotnet sonarscanner end /d:sonar.token="sqp_62bc1789216859d89b6c6b918c41a92010440305"
