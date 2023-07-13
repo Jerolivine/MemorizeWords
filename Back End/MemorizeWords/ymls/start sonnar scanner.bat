7560c8467efac2d4030a00531b6c262006160eac
asentunali <asentunali@gmail.com>
2023-07-13 12:03:20 +0200

    52 - sonarqube folder structure

--- Back End/MemorizeWords/ymls/start sonnar scanner.bat
+++
@@ -1,6 +0,0 @@
-cd..
-cd MemorizeWords
-dotnet tool install --global dotnet-sonarscanner
-dotnet sonarscanner begin /k:"MemorizeWords" /d:sonar.host.url="http://127.0.0.1:9000"  /d:sonar.token="sqp_5ed4e463f092c3fb270d0af53fc57047ea7ad634"
-dotnet build
-dotnet sonarscanner end /d:sonar.token="sqp_5ed4e463f092c3fb270d0af53fc57047ea7ad634"
