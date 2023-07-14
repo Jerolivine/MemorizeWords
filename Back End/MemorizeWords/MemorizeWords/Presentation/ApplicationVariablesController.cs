1fee14b245e364e223c7fcd4174726950d6312e9
hamdiboyraz <hamdiboyraz19@gmail.com>
2023-07-15 02:39:20 +0300

    Add get request for sequent true answer count

---
+++ Back End/MemorizeWords/MemorizeWords/Presentation/ApplicationVariablesController.cs
@@ -0,0 +1,29 @@
+﻿using MemorizeWords.Infrastructure.Presentation;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MemorizeWords.Presentation
+{
+    public class ApplicationVariablesController : BaseController
+    {
+        private readonly IConfiguration _configuration;
+
+        public ApplicationVariablesController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet("app-variables")]
+        public IActionResult GetApplicationVariables()
+        {            
+            // Get the value of "SequentTrueAnswerCount" from app.settings.json
+            int sequentTrueAnswerCount = _configuration.GetValue<int>("SequentTrueAnswerCount");
+
+            var applicationVariables = new
+            {
+                SequentTrueAnswerCount = sequentTrueAnswerCount
+            };
+
+            return Ok(applicationVariables);
+        }
+    }
+}
